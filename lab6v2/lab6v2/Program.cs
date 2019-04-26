6b4a129b10fd518a10f54e157c16f4090e6c95a9
Andre Otte <andre.b.otte1@gmail.com>
2019-04-25 20:27:02 -0400

    there's still a bug...

--- lab6v2/lab6v2/Program.cs
+++ lab6v2/lab6v2/Program.cs
@@ -12,35 +12,64 @@ namespace lab6
         static void Main(string[] args)
         {
             bool play = true;
-            Random rand = new Random();
 
-            Console.WriteLine("enter number of sides: ");
-            int sideNumber = Convert.ToInt16(Console.ReadLine());
-
-            for (int i = 0; i < 10; i++) // Change i < val to change number of random numbers generated
+            while (play)
             {
-                int output = GetRandom(sideNumber, rand);
-                Console.WriteLine(output);
+                Random rando = new Random();
+                GetInput(rando);
+                Console.WriteLine("Would you like to play again? (y/n)");
+                string continuePlaying = Console.ReadLine().ToLower();
+
+                if (continuePlaying == "no" || continuePlaying == "n")
+                {
+                    play = false;
+                }
+            }
+        }
+
+        public static void GetInput(Random rand)
+        {
+            int sideNumber;
+            int diceNumber;
+
+            Console.WriteLine("Enter number of sides each dice will have: ");            
+            string sides = Console.ReadLine();
+            int.TryParse(sides, out sideNumber);
+            Console.WriteLine("Enter the number of dice you want to roll: ");
+            string dice = Console.ReadLine();
+            int.TryParse(dice, out diceNumber);
+
+            if(sideNumber == 0 || diceNumber == 0)
+            {
+                Console.WriteLine("Invalid input. Enter sides and dice number as integers.");
+                GetInput(rand);
             }
 
-
+            Console.WriteLine($"sides: {sides}, dice: {dice}, sideNumer: {sideNumber}, diceNumber: {diceNumber}");
+            string results = Roll(sideNumber, diceNumber, rand);
+            Console.WriteLine(results);
         }
 
-        public static void GetInput()
+        public static int GetRandom(int max, Random rand)
         {
-
-        }
-        public static int GetRandom(int sideNumber, Random rand)
-        {
-            int number = rand.Next(1, sideNumber + 1);
-
+            int number = rand.Next(1, max + 1);
             return number;
         }
 
-        public static void Roll()
+        public static string Roll(int sides, int dice, Random rand)
         {
+            int total = 0;
+            int output;
+            string results = "";
 
+            for (int i = 0; i < dice; i++) // Change i < val to change number of random numbers generated
+            {
+                output = GetRandom(sides, rand);
+                results += ($"Die roll #{i + 1}: {output}\n");
+                total += output;
+            }
+            Console.WriteLine($"The total is: {total}");
+            return results;
         }      
-
     }
 }
